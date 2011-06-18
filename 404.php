d80cbf2d7278b35d82a98ca4d8ef7caaef73edd9
Mike Kohn <kohnmd@gmail.com>
2011-06-18 14:56:23 -0600

    small tweaks

--- 404.php
+++ 404.php
@@ -10,21 +10,15 @@
 		<div id="content">
 			
 			<div class="post-wrap">
-				<h2 class="page-title">404 &ndash; Page Not Found</h2>
-			</div>
-			
-			<div class="post-wrap">
 				<div class="post">
-					<h2>Sorry, that page doesn't exist, sucka.</h2>
-				</div>
+					<h1 class="title">Whatchu lookin' for, Willis?</h1>
+					<p>Mosey along. The page you're looking for doesn't exist.</p>
+					<iframe width="425" height="349" src="http://www.youtube.com/embed/pCrjLVSapII?showinfo=0&rel=0" frameborder="0" allowfullscreen></iframe>
+				</div> <!-- .post -->
 			</div>
 		
 		</div>
 		<!-- END #content -->
-		
-		<!--Begin Sidebar-->
-		<?php get_sidebar(); ?>
-		<!--End Sidebar-->
 		
 	</div>
 	<!--End #main-->
